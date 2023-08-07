98f55b8cd4defff3164496ffad038ac6ed295ca6
aapteev <aapteev@t1-consulting.ru>
2023-08-07 13:05:46 +0500

    fsfds

--- src/main/java/com/test/awesome/repository/DocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.awesome.repository;
-
-import com.test.awesome.entity.Document;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DocumentRepository extends CrudRepository<Document, Long> {
-}
--- src/main/java/com/test/awesome/service/SecurityService.java
+++
@@ -1,8 +0,0 @@
-package com.test.awesome.service;
-
-public class SecurityService {
-
-    public String currentUser() {
-        return "User1";
-    }
-}
